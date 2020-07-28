8087c7fdae085d0497cdc8c535174bfcad775863
Thibaut <thibaut.pernin@gmail.com>
2020-07-28 11:51:18 +0200

    🎉 Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Header msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Header
+    }
+  }
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <div class="header-left">
+        <img src="../assets/logo.png" alt="Xplo.fr" class="header-logo">
+        <div class="header-left-actions">
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown button
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div>
+            <form class="form-inline">
+                <input class=" mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="" type="submit"></button>
+            </form>
+        </div>
+    </div>
+    <button class="btn green full-h header-right-btn uppercase bold">
+        Historique
+    </button>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    props: {
+      msg: String
+    }
+  }
+</script>
